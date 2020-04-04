be2fcf661b8390599317d64e44442b8dcd01a0de
Botaemic <dbot@botaemic.com>
2020-04-04 12:15:30 +0200

    - Addition of Camera Shaders ( CRT scanlines + vignette)

---
+++ Runtime/Shaders/CameraShaderEffect.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Botaemic.Core
+{
+    [ExecuteInEditMode]
+    public class CameraShaderEffect : MonoBehaviour
+    {
+       [SerializeField] private Material _material;
+
+
+        #region Unity Functions
+        void OnRenderImage(RenderTexture source, RenderTexture destination)
+        {
+            Graphics.Blit(source, destination, _material);
+        }
+        #endregion
+
+
+
+        #region Logging
+        private void Log(string text)
+        {
+            DebugUtility.Log(text);
+        }
+
+        private void LogWarning(string text)
+        {
+            DebugUtility.Log("WARNING! " + text);
+        }
+        #endregion
+    }
+}
